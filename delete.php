3f2919aee43943a66f65082b342dae9d954c80ba
sabreensayem <sabrinsayem@gmail.com>
2025-03-05 12:48:46 +0200

    delete.php

--- delete.php
+++ delete.php
@@ -1,19 +1,19 @@
 <?php
- 
- session_start();
- if (isset($_session['role'])&&$_session['role']==='admin'){
-}else{
-    echo"sorry,you do not have permission to delete the data";
-}
 
 //connection
-
 try {
     $id = $_GET['id'];
     $pdo = new pdo("mysql:host=localhost;dbname=BlogDB", "root", "");
+session_start();
+ if (isset($_session['role'])&&$_session['role']==='admin'){
     //query
     $pdo->query("delete from posts where id='$id'");
+
     header("Location:posts.php");
+  }else{
+    echo"sorry,you do not have permission to delete the data";
+}
+
 } catch (PDOException $e) {
     echo $e;
 }
